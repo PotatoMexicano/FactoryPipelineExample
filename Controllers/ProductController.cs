0f8b44066c9a31d914aff1b4c85b14f3e334289a
Potato Mexicano <potatomexicano006@yahoo.com>
2025-01-28 22:47:42 -0300

    fix: Removido User Controller
    * Removido Controller e Repository de Usuários.
    * Adicionado Controller e Repository para Produtos.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Pipeline.Empresa.Constants;
+using Pipeline.Empresa.Entities;
+using Pipeline.Empresa.Pipeline;
+using Pipeline.Empresa.Repositories;
+
+namespace Pipeline.Empresa.Controllers;
+[Route("api/product")]
+[ApiController]
+public class ProductController(IProductRepository repository, IEmpresaFactory empresaFactory) : ControllerBase
+{
+    [HttpGet("{idProduct:int}", Name = "GetProduct")]
+    [Authorize]
+    public async Task<ActionResult<Product?>> GetProduct(Int32 idProduct, [FromQuery] Int32? idEmpresa = null, CancellationToken cancellation = default)
+    {
+        Product? product = await repository.GetProduct(idProduct, cancellation);
+
+        if (product == null) return NotFound(new ProblemDetails { Detail = "Produto não encontrado." });
+
+        IEmpresa empresa = empresaFactory.GetEmpresa(idEmpresa);
+        ProductDTO customDTO = empresa.GenerateDTO(product);
+
+        return Ok(customDTO);
+    }
+
+    [HttpGet]
+    [Authorize]
+    public async Task<ActionResult<ICollection<Product>>> GetProducts([FromQuery] Int32? idEmpresa = null, CancellationToken cancellation = default)
+    {
+        ICollection<Product> products = await repository.GetProducts(cancellation);
+
+        IEmpresa empresa = empresaFactory.GetEmpresa(idEmpresa);
+        ICollection<ProductDTO> customDTOs = empresa.GenerateDTOs(products);
+
+        return Ok(customDTOs);
+    }
+
+    [HttpPost]
+    [Authorize(Roles = RoleConstants.Admin)]
+    public async Task<ActionResult<Product>> RegisterProduct([FromBody] RegisterProductDTO request, CancellationToken cancellation = default)
+    {
+        Product? entity = await repository.RegisterProduct(request, cancellation);
+
+        if (entity == null) return BadRequest(new ProblemDetails { Detail = "Falha ao registrar o produto." });
+
+        return CreatedAtRoute("GetProduct", new { idProduct = entity.Id }, entity);
+    }
+}
---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pipeline.Empresa.Context;
+using Pipeline.Empresa.Entities;
+
+namespace Pipeline.Empresa.Repositories;
+
+public interface IProductRepository
+{
+    Task<Product?> GetProduct(Int32 id, CancellationToken cancellation);
+    Task<ICollection<Product>> GetProducts(CancellationToken cancellation);
+    Task<Product?> RegisterProduct(RegisterProductDTO request, CancellationToken cancellation);
+}
+
+public class ProductRepository(ApplicationDbContext context) : IProductRepository
+{
+    public async Task<Product?> GetProduct(Int32 id, CancellationToken cancellation)
+    {
+        return await context.Products.Where(x => x.Id == id).FirstOrDefaultAsync(cancellation);
+    }
+
+    public async Task<ICollection<Product>> GetProducts(CancellationToken cancellation)
+    {
+        return await context.Products.ToArrayAsync(cancellation);
+    }
+
+    public async Task<Product?> RegisterProduct(RegisterProductDTO request, CancellationToken cancellation)
+    {
+        Product product = new Product { Description = request.Description, Price = request.Price, Quantity = request.Quantity };
+
+        context.Products.Add(product);
+        await context.SaveChangesAsync(cancellation);
+
+        return product;
+    }
+}
